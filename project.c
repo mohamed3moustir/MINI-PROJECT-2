49bd327d40eae965b9c4edbaa6ff6a6061e6da42
mohamed3moustir <mohamedmoustir99@gmail.com>
2024-09-05 20:15:26 +0200

    Update project.c

--- project.c
+++ project.c
@@ -1,5 +1,5 @@
-// Online C compiler to run C program online
 #include <stdio.h>
+#include <string.h>
 struct Contact{
     char nom[100][100];
     char email[100][100];
@@ -11,6 +11,12 @@ int tailler = 0;
 void ajouter_contact();
 // function 2
 void Afficher_contact();
+// funtion 3
+void  Modifier_contact();
+//function 4
+
+//function 5
+void Rechercher_contact();
 int main() {
     struct Contact arg;
     int choix;
@@ -24,24 +30,29 @@ int main() {
      scanf("%d",&choix);
    switch(choix){
        
-       case 1:ajouter_contact();
+       case 1:
+       ajouter_contact();
        break;
         case 2:
+        Modifier_contact();
        break;
         case 3:
        break;
-        case 4:Afficher_contact();
+        case 4:
+        Afficher_contact();
        break;
         case 5:
+        Rechercher_contact();
        break;
          default:
                 printf("Choix invalide!\n");
    
 }
-   }while(choix != 5);
+   }while(choix != 6);
 
     return 0;
 }
+
 
 void ajouter_contact(){
       printf("enter tailler\t");
@@ -55,6 +66,28 @@ void ajouter_contact(){
     scanf(" %[^\n]s",arg.Num_telephone[i]);
 }
 }
+
+
+
+void  Modifier_contact() {
+    char verifie_nom[100];
+    printf("enter nom personne you want to suprimer\t");
+    scanf(" %[^\n]s",verifie_nom);
+    for(int i=0;i<tailler;i++){
+    if(strcmp(verifie_nom,arg.nom[i]) == 0 ){
+     printf("enter nom personne\t");
+    scanf(" %[^\n]s",arg.nom[i]);
+     printf("enter email\t");
+    scanf(" %[^\n]s",arg.email[i]);
+     printf("enter  telephone\t");
+    scanf(" %[^\n]s",arg.Num_telephone[i]);
+
+    }
+    else
+    printf(" ERROR\n");
+}
+}
+
 void Afficher_contact(){
     for(int i=0;i<tailler;i++){
     printf("nom est %s ||email est %s  ||telephone %s  \n",arg.nom[i],arg.email[i],arg.Num_telephone[i]);
@@ -64,15 +97,21 @@ void Afficher_contact(){
 
 
 
+void Rechercher_contact(){
+ char verifie_nom[100];
+    printf("enter nom personne you want to Rechercher\t");
+    scanf(" %[^\n]s",verifie_nom);
+    for(int i=0;i<tailler;i++){
+    if(strcmp(verifie_nom,arg.nom[i]) == 0 ){
+       for(int i=0;i<tailler;i++){
+    printf("nom est %s ||email est %s  ||telephone %s  \n",arg.nom[i],arg.email[i],arg.Num_telephone[i]);
+    
+    }
+    }
 
+}
 
-
-
-
-
-
-
-
-
+    
+}
 
 
